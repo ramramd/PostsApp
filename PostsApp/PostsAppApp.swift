3f926e8e1fde96d9d654895aab7d68b764e1d5f8
Naveen Ramachandrappa <naveen_ramachandrappa@apple.com>
2023-07-31 11:57:52 -0700

    Initial Commit

---
+++ PostsApp/PostsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsAppApp.swift
+//  PostsApp
+//
+//  Created by Naveen Ramachandrappa on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct PostsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
